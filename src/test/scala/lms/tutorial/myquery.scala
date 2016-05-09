2b1ccc1374b0e1857436ab4e28634b548bd29e03
XilunWu <wu636@purdue.edu>
2016-05-09 15:32:20 -0400

    test

--- src/test/scala/lms/tutorial/myquery.scala
+++ src/test/scala/lms/tutorial/myquery.scala
@@ -115,7 +115,7 @@ class LFTjoinQueryTest extends TutorialFunSuite {
     val scan_supplier = Scan("../../../data/supplier.tbl",Some(Schema("#SUPPKEY","S_NAME","S_ADDRESS","#NATIONKEY","S_PHONE","S_ACCTBAL","S_COMMENT")),Some('\t'))
     
     val expectedAstForTest = Map(
-      "lftj_q5" -> Group(Schema("N_NAME"), Schema("#COUNT"),  //Here we need hack Group to support count(*)
+      "lftj_q5" -> /*Group(Schema("N_NAME"), Schema("#COUNT"),  //Here we need hack Group to support count(*)
         LFTJoin(List(
           Project(Schema("#NATIONKEY","N_NAME","#REGIONKEY"), Schema("#NATIONKEY","N_NAME","#REGIONKEY"), scan_nation),
           Project(Schema("#REGIONKEY"), Schema("#REGIONKEY"), Filter(Eq(Field("R_NAME"), Value("ASIA")), scan_region)),
@@ -126,7 +126,7 @@ class LFTjoinQueryTest extends TutorialFunSuite {
           Project(
             Schema("#CUSTKEY","#NATIONKEY"),
             Schema("#CUSTKEY","#NATIONKEY"),
-            scan_customer)/*,
+            scan_customer),
           Project(
             Schema("#ORDERKEY","#CUSTKEY"), 
             Schema("#ORDERKEY","#CUSTKEY"), 
@@ -136,8 +136,9 @@ class LFTjoinQueryTest extends TutorialFunSuite {
           Project(
             Schema("#ORDERKEY","#PARTKEY","#SUPPKEY"),
             Schema("#ORDERKEY","#PARTKEY","#SUPPKEY"),
-            scan_lineitem)*/
-        )))
+            scan_lineitem)
+        )))*/
+        Project(Schema("#NATIONKEY","N_NAME","#REGIONKEY"), Schema("#NATIONKEY","N_NAME","#REGIONKEY"), scan_nation)
     )
   }
 
